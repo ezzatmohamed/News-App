f4ee3d23f94697c6babc1ea394e0ab468e86d360
ezzatmohamed <ezzat.mohamed2021@gmail.com>
2020-12-06 22:33:56 +0200

    added create favorite tool

--- server/app/Providers/NovaServiceProvider.php
+++ server/app/Providers/NovaServiceProvider.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Gate;
 use Laravel\Nova\Cards\Help;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
-
+use Newsapp\CreateFavorite\CreateFavorite;
 class NovaServiceProvider extends NovaApplicationServiceProvider
 {
     /**
@@ -79,6 +79,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         return [
             new \Yadahan\BouncerTool\BouncerTool(),
+            new CreateFavorite
 
         ];
     }
---
+++ server/nova-components/CreateFavorite/resources/js/tool.js
@@ -0,0 +1,9 @@
+Nova.booting((Vue, router, store) => {
+  router.addRoutes([
+    {
+      name: 'create-favorite',
+      path: '/create-favorite',
+      component: require('./components/Tool'),
+    },
+  ])
+})
