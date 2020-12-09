d7ffc0ce2b1a41f87461c64ee69def1e1a0df73d
ezzatmohamed <ezzat.mohamed2021@gmail.com>
2020-12-09 15:50:53 +0200

    added table component and rows components

--- server/nova-components/FavoritesGrid/resources/js/components/Tool.vue
+++ server/nova-components/FavoritesGrid/resources/js/components/Tool.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-
+        <favorites-table />
     </div>
 </template>
 
---
+++ server/nova-components/FavoritesGrid/resources/js/components/tables/favoritesTable/favoritesTable.vue
@@ -0,0 +1,48 @@
+
+<template>
+    <div>
+      <table class = "table-container">
+
+        <row :cells="colsData" :isHeader="true"/>
+        <row v-for="data in rowData" :key="data.id" :cells="data"/>
+      </table>
+    </div>
+</template>
+
+<script>
+  import './favoritesTable.css';
+  import row from './../../row/row'
+
+  export default {
+    name:"favorites-table",
+    components:{row},
+    data(){
+      return{
+        rowData:[
+          {
+            id:1,
+            title:2,
+            url:3,
+            image:4,
+            description:5
+          },
+          {
+            id:1,
+            title:2,
+            url:3,
+            image:4,
+            description:3,
+          }
+        ],
+        colsData:{
+            id:"ID",
+            title:"Title",
+            Url:"Url",
+            Image:"Image",
+            Description:"Description"
+        }
+      }
+    },
+  }
+</script>
+ 
