969edc5b09264374d7f145de0f650e36d5d38065
ezzatmohamed <ezzat.mohamed2021@gmail.com>
2020-12-22 15:08:54 +0200

    init2

---
+++ server/tests/Browser/ExampleTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\Models\User;
+class ExampleTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        // $response = $this->get('nova/login');
+        // $response->assertSee('Welcome Back!');
+
+        $this->browse(function (Browser $browser) {
+            // $browser->loginAs(User::find(1))
+            //         ->visit('/nova/create-favorite')
+            //         ->text('url','www.google3.com')
+            //         ->select('user','1')
+            //         ->press('Create');
+            $browser->visit('/nova/login')
+                    ->assertSee('Welcome Back!');
+        });
+    }
+}
